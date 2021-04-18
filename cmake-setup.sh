34107504f4757da6fe557463be29f2a7a0860513
hjw21century <hjw21century@163.com>
2021-04-18 18:29:10 +0800

    cmake

---
+++ cmake-setup.sh
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_PREFIX_PATH /usr/local)
+#set this variable, so that find_package(), find_program(), find_library(), find_path() and find_file() will search the prefix for other software. 
+
+
+set(CMAKE_INSTALL_PREFIX /selflibs)
+#set this variable, so that tell where the installation will locate
+
