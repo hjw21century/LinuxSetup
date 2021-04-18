7e04dd0975a113435e9916d1121a582b9bf740a8
hjw21century <hjw21century@163.com>
2021-04-18 17:24:03 +0800

    initial

---
+++ compiler-setup.sh
@@ -0,0 +1,8 @@
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 7
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 7
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
+
+sudo update-alternatives --config gcc
+
